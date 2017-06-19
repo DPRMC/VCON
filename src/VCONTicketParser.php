cb3fb7b1e0b7d4e5b7ae68536c9f4ef2749d7c74
Michael Drennen <michaeldrennen74@gmail.com>
2017-06-19 15:40:30 -0600

    Commit

--- src/VCONTicketParser.php
+++ src/VCONTicketParser.php
@@ -1,5 +1,6 @@
 <?php
 namespace DPRMC\VCON;
+use Exception;
 
 class VCONTicketParser{
     /**
@@ -22,17 +23,31 @@ class VCONTicketParser{
     public function parse($vconText){
         $this->text = $vconText; // Not sure I need to save this as a property.
         $vconTicket = new VCONTicket();
-        $vconTicket->factor = $this->parseFactor($vconText);
+        try{
+            $vconTicket->factor = $this->parseFactor($vconText);
+        } catch(Exception $e) {
 
+        }
 
         return $vconTicket;
     }
 
+    /**
+     * @param $text
+     * @return float
+     * @throws Exception
+     */
     protected function parseFactor($text){
-        $factor = null;
         $pattern = '/Factor\s*(.*)>/';
         preg_match($pattern,$text,$matches);
-        print_r($matches);
-        return $factor;
+
+        if(sizeof($matches) != 2 ){
+            throw new Exception("parseFactor() failed because it didn't find a factor. (Or found too many...)");
+        }
+
+        // http://php.net/manual/en/function.preg-match.php
+        $factor = $matches[1];
+
+        return (float)$factor;
     }
 }
--- tests/VCONTicketParserTest.php
+++ tests/VCONTicketParserTest.php
@@ -1,10 +1,9 @@
 <?php
+
 namespace DPRMC\VCON;
 
 use PHPUnit\Framework\TestCase;
 use DPRMC\VCON\VCONTicketParser;
-
-
 
 
 class VCONTicketParserTest extends TestCase {
@@ -12,12 +11,10 @@ class VCONTicketParserTest extends TestCase {
     /**
      * @var array
      */
-    protected $ticketUrls = [
-        0 => 'https://raw.githubusercontent.com/DPRMC/VCON/master/tests/testTickets/ticket_1.txt',
-        1 => 'https://raw.githubusercontent.com/DPRMC/VCON/master/tests/testTickets/ticket_2.txt',
-        2 => 'https://raw.githubusercontent.com/DPRMC/VCON/master/tests/testTickets/ticket_3.txt',
-        3 => 'https://raw.githubusercontent.com/DPRMC/VCON/master/tests/testTickets/ticket_4.txt',
-    ];
+    protected $ticketUrls = [ 0 => 'https://raw.githubusercontent.com/DPRMC/VCON/master/tests/testTickets/ticket_1.txt',
+                              1 => 'https://raw.githubusercontent.com/DPRMC/VCON/master/tests/testTickets/ticket_2.txt',
+                              2 => 'https://raw.githubusercontent.com/DPRMC/VCON/master/tests/testTickets/ticket_3.txt',
+                              3 => 'https://raw.githubusercontent.com/DPRMC/VCON/master/tests/testTickets/ticket_4.txt', ];
 
     /**
      * @var array
@@ -29,21 +26,34 @@ class VCONTicketParserTest extends TestCase {
      *
      */
     public function setUp() {
-        foreach($this->ticketUrls as $i => $ticketUrl):
-            $this->ticketTexts[$i] = file_get_contents($ticketUrl);
+        foreach ( $this->ticketUrls as $i => $ticketUrl ):
+            $this->ticketTexts[ $i ] = file_get_contents( $ticketUrl );
         endforeach;
     }
 
     public function testParseFactor() {
-        foreach($this->ticketTexts as $text){
-            $ticketParser = new VCONTicketParser();
-            $ticket = $ticketParser->parse($text);
-        }
+        $index = 0;
+        $factorForIndex = 0.9996246086;
+        $text = $this->ticketTexts[ $index ];
 
+        $ticketParser = new VCONTicketParser();
+        $ticket = $ticketParser->parse( $text );
+
+        $this->assertEquals( $factorForIndex, $ticket->factor );
 
     }
 
+    public function testMissingFactor() {
+        $index = 2;
+        $factorForIndex = NULL;
+        $text = $this->ticketTexts[ $index ];
 
+        $ticketParser = new VCONTicketParser();
+        $ticket = $ticketParser->parse( $text );
+
+        $this->assertEquals( $factorForIndex, $ticket->factor );
+
+    }
 
 
 }
