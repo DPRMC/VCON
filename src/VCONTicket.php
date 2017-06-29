85aa0801c0072ec24a5448d73d1b70258c8ceec1
Michael Drennen <michaeldrennen74@gmail.com>
2017-06-29 08:29:00 -0600

    Tons of changes from the dev platform.

--- src/VCONTicket.php
+++ src/VCONTicket.php
@@ -10,7 +10,14 @@ class VCONTicket{
 
     public $rawText;
 
+    /**
+     * @var string The CUSIP, ISIN, or some other type of identifier.
+     */
     public $identifier;
+
+    /**
+     * @var string The type of identifier used for this security. See constants above.
+     */
     public $identifierType;
 
     /**
@@ -18,19 +25,57 @@ class VCONTicket{
      */
     public $identifierMissing = true;
 
+    /**
+     * @var string The trader responsible for this ticket.
+     */
     public $trader;
+
+    /**
+     * @var string The ISIN, if this trade ticket references one. For example: US0378331005
+     */
     public $isin;
+
+    /**
+     * @var string The CUSIP, if this trade ticket references one. For example: 12669CF96
+     */
     public $cusip;
+
+    /**
+     * @var string The size or quantity of the security bought or sold.
+     * @todo Right now this is a string, and I should convert this to a number.
+     */
     public $quantity;
+
+    /**
+     * @var string The name of the deal/security. For example: CWALT 2002-8 B3
+     */
     public $dealName;
-    //
-    //
+
+    /**
+     * @var string The price of the security for this transaction.
+     */
     public $price;
+
+    /**
+     * @var Carbon The date this transaction should settle on.
+     */
     public $settleDate;
+
+    /**
+     * @var
+     */
     public $nextPaymentDate;
+
+    /**
+     * @var
+     */
     public $dayDelay; // zero day delay?
     public $currentFace;
     public $factorDate;
+
+    /**
+     * @var float The factor of the security at the time of the trade.
+     */
     public $factor;
     public $principalValue;
     //
